c917354b099b82acf2b0ec1902f512a151337706
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2026-05-29 23:52:59 +0200

    Added base script

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -jar voleurs_de_tresors.jar -player1 "uv run main.py" -player2 "uv run main.py"
+java -jar voleurs_de_tresors.jar -player1 "uv run src/main.py" -player2 "uv run src/main.py"
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 java -jar voleurs_de_tresors.jar \
-    -player1 "uv run main.py" \
-    -player2 "uv run main.py"
+    -player1 "uv run src/main.py" \
+    -player2 "uv run src/main.py"
